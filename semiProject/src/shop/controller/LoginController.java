fcb2e637008ce5ff7a354fccc91f1b7bfd0037f5
leewontae <whsslfp111@naver.com>
2021-05-29 01:57:40 +0900

    db 연결 성공 db 아이디 : shopdb

---
+++ semiProject/src/shop/controller/LoginController.java
@@ -0,0 +1,45 @@
+package shop.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import shop_dao.Membersdao;
+import test.db.MyDBCP;
+
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+			String id = req.getParameter("id");
+		
+			String pwd = req.getParameter("pwd");
+	
+			Membersdao dao = Membersdao.getinstance();
+			boolean find =  dao.isMember(id, pwd);
+			if(find) {
+				HttpSession session = req.getSession();
+				session.setAttribute("id", id);
+				resp.sendRedirect(req.getContextPath()+"/result.jsp");
+			}else {
+				resp.sendRedirect(req.getContextPath()+"/shop/login.jsp");
+			
+			}
+
+		
+	}
+
+}
---
+++ semiProject/src/shop_dao/Membersdao.java
@@ -0,0 +1,44 @@
+package shop_dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import test.db.MyDBCP;
+
+public class Membersdao {
+
+	private static Membersdao instance= new Membersdao();
+	private Membersdao() {};
+	public static Membersdao getinstance() {
+		return instance;
+	}
+	
+	public boolean isMember(String id , String pwd) {
+		Connection con = null;
+		PreparedStatement pstmt = null; 
+		ResultSet rs= null; 
+		try {
+		
+			con = MyDBCP.getConnection();
+			String sql="select * from members where id=? and pwd=?";
+			System.out.println("memberdao");
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2,pwd);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				
+				return true;
+			}else {
+				return false;
+			}
+			
+		}catch(SQLException s) {
+			s.getMessage();
+			return false;
+			
+		}
+	}
+}
