60a0471181b98002e6523af6b237b4f74c2c27c3
leechanho <officialee1993@gmail.com>
2021-05-31 23:04:53 +0900

    ...

--- semiProject/src/shop/controller/LoginController.java
+++ semiProject/src/shop/controller/LoginController.java
@@ -32,19 +32,20 @@ public class LoginController extends HttpServlet{
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-
 			String id = req.getParameter("id");
-		
 			String pwd = req.getParameter("pwd");
-	
+
 			Membersdao dao = Membersdao.getinstance();
 			boolean find =  dao.isMember(id, pwd);
+			
+			
+			
 			if(find) {
 				HttpSession session = req.getSession();
 				session.setAttribute("id", id);
 				resp.sendRedirect(req.getContextPath()+"/shop/index");
 			}else {
+
 				resp.sendRedirect(req.getContextPath()+"/shop/login");
 			}	
 	}
