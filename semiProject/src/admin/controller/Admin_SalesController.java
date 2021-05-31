d5195fff3dd3256797cb73a9c7afb9fe5f5a239a
leechanho <officialee1993@gmail.com>
2021-05-31 18:36:43 +0900

    관리자 매출통계 페이지 추가

---
+++ semiProject/src/admin/controller/Admin_SalesController.java
@@ -0,0 +1,19 @@
+package admin.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/admin/sales")
+public class Admin_SalesController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("sidemenu", "/admin/sidemenu.jsp");
+		req.setAttribute("content", "/admin/sales_statistics.jsp");
+
+		req.getRequestDispatcher("/admin/index.jsp").forward(req, resp);
+	}
+}
