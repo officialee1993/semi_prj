41eddde7a333bf3211aa64168a94c702a7951152
hylovewb <hylovewb@gmail.com>
2021-06-07 16:56:49 +0900

    admin 회원관리 리스트업 및 회원삭제

--- semiProject/src/admin/controller/Admin_MembersListController.java
+++ semiProject/src/admin/controller/Admin_MembersListController.java
@@ -1,17 +1,25 @@
 package admin.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import shop.vo.Members_vo;
+import shop_dao.Membersdao;
 @WebServlet("/admin/members_list")
 public class Admin_MembersListController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Membersdao dao = Membersdao.getinstance();
+		ArrayList<Members_vo> all_members = new ArrayList<Members_vo>();
+		all_members = dao.all_members();
 		
+		req.setAttribute("all_members", all_members);
 		
 		req.setAttribute("sidemenu", "/admin/sidemenu.jsp");
 		req.setAttribute("content", "/admin/members_list.jsp");
