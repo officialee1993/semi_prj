0d7fe6518463e5a02f3a4e22e0415f2635702505
leewontae <whsslfp111@naver.com>
2021-06-09 14:23:31 +0900

    매출 통계 수정 완료

--- semiProject/src/admin/controller/Admin_SalesList_Search_Controller.java
+++ semiProject/src/admin/controller/Admin_SalesList_Search_Controller.java
@@ -14,118 +14,107 @@ import javax.servlet.http.HttpServletResponse;
 
 import shop.vo.Sales_stats_vo;
 import shop_dao.Salesdao;
+
 @WebServlet("/admin/sales_search_list")
-public class Admin_SalesList_Search_Controller extends HttpServlet{
-	
+public class Admin_SalesList_Search_Controller extends HttpServlet {
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-	
+
 		req.setCharacterEncoding("utf-8");
-		ArrayList<Sales_stats_vo>  list = null; 
+		ArrayList<Sales_stats_vo> list = null;
 		String todays = req.getParameter("todays");
 		String months = req.getParameter("months");
 		String years = req.getParameter("years");
-		
-		System.out.println(todays + " " +months+ " "+ years);
-		
+
+		System.out.println(todays + " " + months + " " + years);
+
 		Salesdao salesdao = Salesdao.getinstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar cal = Calendar.getInstance();
-        String strToday = sdf.format(cal.getTime());
-        int all_sum = 0; 
-        
-		if(todays.equals("1")&&months.equals("undefined")&&years.equals("undefined")) {
-			
-			
-	        String strTodayend = strToday+"-23-59";
-	       
-			list =  salesdao.Sales_stats_kal_select(strToday, strTodayend);
-			for(Sales_stats_vo vo : list) {
-				all_sum +=vo.getAll_Sales();
-				
-			}
-			//System.out.println("all_sum" + all_sum);
-			
-			req.setAttribute("all_sum", all_sum);
-			req.setAttribute("list", list);
-			
-	       //System.out.println("Today=" + strToday);
+		Calendar cal = Calendar.getInstance();
+		String strToday = sdf.format(cal.getTime());
+		int all_sum = 0;
 
-	       
-		}else if(todays.equals("undefined")&&months.equals("2")&&years.equals("undefined")) {
-			
-			String strmonths  = strToday.substring(0,8)+"01";
-			
-			int year = Integer.parseInt(strToday.substring(0,4));
-			int month = Integer.parseInt(strToday.substring(5,7));
-			int day = Integer.parseInt(strToday.substring(8,10));
-			System.out.println(year + " "+month + " "+day);
-			
-			cal.set(year, month-1, day);
+		if (todays.equals("1") && months.equals("undefined") && years.equals("undefined")) {
+
+			String strTodayend = strToday + "-23-59";
+
+			list = salesdao.Sales_stats_kal_select(strToday, strTodayend);
+			for (Sales_stats_vo vo : list) {
+				all_sum += vo.getAll_Sales();
+
+			}
+			System.out.println("all_sum" + all_sum);
+
+			req.setAttribute("all_sum", all_sum);
+			req.setAttribute("list", list);
+
+			// System.out.println("Today=" + strToday);
+
+		} else if (todays.equals("undefined") && months.equals("2") && years.equals("undefined")) {
+
+			String strmonths = strToday.substring(0, 8) + "01";
+
+			int year = Integer.parseInt(strToday.substring(0, 4));
+			int month = Integer.parseInt(strToday.substring(5, 7));
+			int day = Integer.parseInt(strToday.substring(8, 10));
+			System.out.println(year + " " + month + " " + day);
+
+			cal.set(year, month - 1, day);
 			int lastmonthday = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-			
-			String strmonthsend  = strToday.substring(0,8)+lastmonthday+"-23-59";
-			
-			
-			System.out.println(strmonths +" "+ strmonthsend);
-			
-			list =  salesdao.Sales_stats_kal_select(strmonths, strmonthsend);
-			for(Sales_stats_vo vo : list) {
-				all_sum +=vo.getAll_Sales();
-				
+
+			String strmonthsend = strToday.substring(0, 8) + lastmonthday + "-23-59";
+
+			System.out.println(strmonths + " " + strmonthsend);
+
+			list = salesdao.Sales_stats_kal_select(strmonths, strmonthsend);
+			for (Sales_stats_vo vo : list) {
+				all_sum += vo.getAll_Sales();
+
 			}
-			//System.out.println("all_sum" + all_sum);
+			System.out.println("all_sum" + all_sum);
 			req.setAttribute("all_sum", all_sum);
 			req.setAttribute("list", list);
-			
-		}else if(todays.equals("undefined")&&months.equals("undefined")&&years.equals("3")) {
-			
-			String stryears  = strToday.substring(0,5)+"01-01";
-			String stryearsend  = strToday.substring(0,5)+"12-31";
-			
-			list =  salesdao.Sales_stats_kal_select(stryears, stryearsend);
-			for(Sales_stats_vo vo : list) {
-				all_sum +=vo.getAll_Sales();
-				
+
+		} else if (todays.equals("undefined") && months.equals("undefined") && years.equals("3")) {
+
+			String stryears = strToday.substring(0, 5) + "01-01";
+			String stryearsend = strToday.substring(0, 5) + "12-31";
+
+			list = salesdao.Sales_stats_kal_select(stryears, stryearsend);
+			for (Sales_stats_vo vo : list) {
+				all_sum += vo.getAll_Sales();
+
 			}
-			//System.out.println("all_sum" + all_sum);
+			System.out.println("all_sum" + all_sum);
 			req.setAttribute("all_sum", all_sum);
 			req.setAttribute("list", list);
-		}else {
-			
+		} else {
+
 			String beforekal = req.getParameter("beforekal");
 			String afterkal = req.getParameter("afterkal");
-			String afterkal2 = afterkal+"-23-59";
-			
-			list =  salesdao.Sales_stats_kal_select(beforekal, afterkal2);
+			String afterkal2 = afterkal + "-23-59";
+
+			list = salesdao.Sales_stats_kal_select(beforekal, afterkal2);
 			req.setAttribute("list", list);
-			
+
 		}
-		
-		
-		
-		
-		
+
 		PrintWriter pw = resp.getWriter();
 		resp.setContentType("text/xml;charset=utf-8");
 		pw.print("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
 		pw.print("<result>");
-		for(Sales_stats_vo vo : list) {
-			
+		for (Sales_stats_vo vo : list) {
+
 			pw.print("<comm>");
-			pw.print("<sales_stats_num>"+vo.getSales_stats_num()+"</sales_stats_num>");
-			pw.print("<all_sales>"+vo.getAll_Sales()+"</all_sales>");
-			pw.print("<o_date>"+vo.getO_date()+"</o_date>");
-			pw.print("<p_count>"+vo.getP_count()+"</p_count>");
-			pw.print("<p_num>"+vo.getP_num()+"</p_num>");
+			pw.print("<sales_stats_num>" + vo.getSales_stats_num() + "</sales_stats_num>");
+			pw.print("<all_sales>" + vo.getAll_Sales() + "</all_sales>");
+			pw.print("<o_date>" + vo.getO_date() + "</o_date>");
+			pw.print("<p_count>" + vo.getP_count() + "</p_count>");
+			pw.print("<p_num>" + vo.getP_num() + "</p_num>");
 			pw.print("</comm>");
 		}
 		pw.print("</result>");
-		
 
-		
-		
-		
 	}
 }
